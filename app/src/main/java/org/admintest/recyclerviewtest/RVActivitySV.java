07cf469fbff04b1948a14d6ec22f133637e824ba
lognaturel <lognaturel@gmail.com>
2018-08-23 10:21:16 -0700

    Cap RecyclerView height for long list

--- app/src/main/java/org/admintest/recyclerviewtest/RVActivitySV.java
+++ app/src/main/java/org/admintest/recyclerviewtest/RVActivitySV.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.DisplayMetrics;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
 import java.util.Arrays;
@@ -18,13 +20,21 @@ public class RVActivitySV extends AppCompatActivity {
         setContentView(R.layout.activity_rv_sv);
 
         List<String> items = Arrays.asList(getResources().getStringArray(R.array.words));
+        //List<String> items = Arrays.asList(getResources().getStringArray(R.array.less_than_200_words));
 
-        RecyclerView recyclerView = new RecyclerView(this);
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(true);
+
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
         recyclerView.setAdapter(new RVAdapter(items));
 
-        ((FrameLayout) findViewById(R.id.recyclerView)).addView(recyclerView);
+        // If we have more than 200 elements, only let the RecyclerView take up 80% of the screen height
+        if (recyclerView.getAdapter().getItemCount() > 200) {
+            DisplayMetrics displayMetrics = new DisplayMetrics();
+            getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+
+            recyclerView.getLayoutParams().height = (int) (displayMetrics.heightPixels * .80);
+        }
     }
 }
